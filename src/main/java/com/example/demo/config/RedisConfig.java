ad76a34aa78bcc1a812a7fed16d1088aa94675c4
Thanh Huynh <thanhhuynhduy@gmail.com>
2025-04-17 17:05:08 +0700

    9th commit

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,26 +1,33 @@
 package com.example.demo.config;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Configuration
-public class RedisConfig {
+@EnableCaching
+public class RedisConfig implements CachingConfigurer {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        // redisStandaloneConfiguration.setHostName("127.0.0.1");
-        // redisStandaloneConfiguration.setPort(6379);
+        redisStandaloneConfiguration.setHostName("127.0.0.1");
+        redisStandaloneConfiguration.setPort(6379);
         // redisStandaloneConfiguration.setDatabase(1);
-        // redisStandaloneConfiguration.setUsername("password");
-        // redisStandaloneConfiguration.setPassword("password");
+        redisStandaloneConfiguration.setPassword("redisdemo");
         return new JedisConnectionFactory (redisStandaloneConfiguration);
     }
 
@@ -28,12 +35,19 @@ public class RedisConfig {
     public RedisTemplate<UUID, Object> redisTemplate() {
         RedisTemplate<UUID, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
-        template.setValueSerializer(new JdkSerializationRedisSerializer());
-        template.setEnableDefaultSerializer(true);
-        template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(30)) //Time to live cua cache
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())) //Kieu cua key
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())) //Kieu cua value
+                .disableCachingNullValues();
+        return RedisCacheManager.builder(jedisConnectionFactory())
+                .cacheDefaults(config)
+                .build();
     }
 }
--- src/main/java/com/example/demo/service/AntiHeroService.java
+++ src/main/java/com/example/demo/service/AntiHeroService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.UUID;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.AntiHeroEntity;
@@ -16,23 +18,28 @@ public class AntiHeroService {
 
     private AntiHeroRepository repo;
 
+    @Cacheable(value = "heroes")
     public Iterable<AntiHeroEntity> getAllAntiHeroes() {
         return repo.findAll();
     }
 
+    @Cacheable(value = "heroes", key = "#id")
     public AntiHeroEntity getAntiHeroById(UUID id) {
         return findOrThrow(id);
     }
 
+    @CacheEvict(value = "heroes", key = "#id")
     public void removeAntiHeroById(UUID id) {
         findOrThrow(id);
         repo.deleteById(id);
     }
 
+    @CacheEvict(value = "heroes", key = "#antiHero.id")
     public AntiHeroEntity saveAntiHero(AntiHeroEntity antiHero) {
         return repo.save(antiHero);
     }
 
+    @CacheEvict(value = "heroes", key = "#id")
     public void updateAntiHero(UUID id, AntiHeroEntity antiHero) {
         findOrThrow(id);
         repo.save(antiHero);
